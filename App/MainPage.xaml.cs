b638543ea2410e92e967a9220de8435d15e82f47
Khanhvnth <khanhvuong987@gmail.com>
2020-09-17 13:47:16 +0700

    App

---
+++ App/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using App.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<NewItems> NewItems;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            NewItems = new ObservableCollection<NewItems>();
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Music.IsSelected)
+            {
+                NewManager.GetItems("Music", NewItems);
+                TitleTextBlock.Text = "Music";
+            }else if (Food.IsSelected)
+            {
+                NewManager.GetItems("Food", NewItems);
+                TitleTextBlock.Text = "Food";
+            }
+        }
+
+        public void Page_Loaded(Object sender, RoutedEventArgs e)
+        {
+            Music.IsSelected = true;
+        }
+    }
+}
---
+++ App/Model/NewManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Model
+{
+    class NewManager
+    {
+        public static List<NewItems> GetNewItems()
+        {
+            var items = new List<NewItems>();
+
+            items.Add(new NewItems() { Id = 1, Category = "Music", Title = "Adversity to him to saw", Image = "Assets/music1.jpeg", Content = "Land been nor was alone passed though old he yet" });
+            items.Add(new NewItems() { Id = 2, Category = "Music", Title = "Adversity to him to saw", Image = "Assets/music2.jpeg", Content = "Land been nor was alone passed though old he yet" });
+            items.Add(new NewItems() { Id = 3, Category = "Music", Title = "Adversity to him to saw", Image = "Assets/music3.jpeg", Content = "Land been nor was alone passed though old he yet" });
+            items.Add(new NewItems() { Id = 4, Category = "Music", Title = "Adversity to him to saw", Image = "Assets/music4.jpeg", Content = "Land been nor was alone passed though old he yet" });
+            items.Add(new NewItems() { Id = 5, Category = "Music", Title = "Adversity to him to saw", Image = "Assets/music5.jpeg", Content = "Land been nor was alone passed though old he yet" });
+            items.Add(new NewItems() { Id = 6, Category = "Music", Title = "Adversity to him to saw", Image = "Assets/music6.jpeg", Content = "Land been nor was alone passed though old he yet" });
+
+
+            items.Add(new NewItems() { Id = 7, Category = "Food", Title = "Adversity to him to saw", Image = "Assets/Food1.png", Content = "Land been nor was alone passed though old he yet" });
+            items.Add(new NewItems() { Id = 8, Category = "Food", Title = "Adversity to him to saw", Image = "Assets/Food2.png", Content = "Land been nor was alone passed though old he yet" });
+            items.Add(new NewItems() { Id = 9, Category = "Food", Title = "Adversity to him to saw", Image = "Assets/Food3.png", Content = "Land been nor was alone passed though old he yet" });
+            items.Add(new NewItems() { Id = 10, Category = "Food", Title = "Adversity to him to saw", Image = "Assets/Food4.png", Content = "Land been nor was alone passed though old he yet" });
+            items.Add(new NewItems() { Id = 11, Category = "Food", Title = "Adversity to him to saw", Image = "Assets/Food5.png", Content = "Land been nor was alone passed though old he yet" });
+
+
+            return items;
+
+        }
+
+        public static void GetItems(
+            string category,ObservableCollection<NewItems> newItems)
+        {
+            var allItems = GetNewItems();
+
+            var fillteredNewsItems = allItems
+                .Where(p => p.Category == category).ToList();
+
+            newItems.Clear();
+
+            fillteredNewsItems.ForEach(p => newItems.Add(p));
+        }
+    }
+}
